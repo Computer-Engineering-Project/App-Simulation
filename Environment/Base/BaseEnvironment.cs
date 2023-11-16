921b28b659982d67db6f167ac7df76e127b865d4
nguyentuananh <nguyentuananh.hcm@blueocean.net.vn>
2023-11-16 21:59:34 +0700

    re push main

--- Environment/Base/BaseEnvironment.cs
+++ Environment/Base/BaseEnvironment.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Environment.Model;
+using Environment.Service.Interface;
+using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,57 +14,86 @@ namespace Environment.Base
     {
         public List<string> Ports = new List<string>();
         public List<SerialPort> SerialPorts = new List<SerialPort>();
-        public BaseEnvironment()
+        public List<ObjectGoIn> HardwareGoIn = new List<ObjectGoIn>();
+        private readonly ICommunicationService communicationService;
+
+        public BaseEnvironment(ICommunicationService communicationService)
         {
+            this.communicationService = communicationService;
             SetUp();
-            /*            StartPort();*/
         }
 
         private void SetUp()
         {
             var ports = SerialPort.GetPortNames();
-            foreach (var port in ports)
-            {
-                Ports.Add(port);
-            }
+            Ports.AddRange(ports);
         }
         public void StartPort(string port)
         {
             var serialPort = new SerialPort(port, 115200);
             SerialPorts.Add(serialPort);
         }
-
         public void ActiveHardwareDevice(string port)
+        {
+            var serialport = SerialPorts.FirstOrDefault(e => e.PortName == port);
+            if (serialport != null)
+            {
+                var stringActive = Helper.createStringActiveHardware();
+                if (stringActive != null)
+                {
+                    serialport.Write(stringActive, 0, stringActive.Length);
+                }
+                return;
+            }
+        }
+        public string ExecuteReadConfigFromHardware(string port)
+        {
+            //return id : string + module type: string
+            var serialport = SerialPorts.FirstOrDefault(s => s.PortName == port);
+            if (serialport != null)
+            {
+                return Helper.getConfigFromHardware(serialport);
+
+            }
+            return "001:E32";
+        }
+        public void ClosePort(string port)
         {
             foreach (var serialport in SerialPorts)
             {
                 if (serialport.PortName == port)
                 {
-                    var stringActive = Helper.createStringActiveHardware();
-                    if (stringActive != null)
-                    {
-                        serialport.Write(stringActive, 0, stringActive.Length);
-                    }
+                    serialport.Close();
                 }
             }
-
         }
-        public string ExecuteReadConfigFromHardware(string port)
+        private void createSerialPortInitial()
         {
-            //return id : string + module type: string
-            foreach(var serialport in SerialPorts)
+            foreach (var serialport in SerialPorts)
             {
-                if(serialport.PortName == port)
+                if (!serialport.IsOpen)
                 {
-                    //Get Id, type module by Helper function
+                    serialport.Open();
+                    var objectIn = new ObjectGoIn()
+                    {
+                        serialport = serialport,
+                    };
+                    HardwareGoIn.Add(objectIn);
                 }
             }
-            return "";
+            foreach (var packet in HardwareGoIn)
+            {
+                packet.serialport.DataReceived += new SerialDataReceivedEventHandler(addToQueue);
+            }
         }
-        public void ExecuteConfigDevice(string port)
+        private void addToQueue(object sender, SerialDataReceivedEventArgs e)
         {
-
+            /*byte[] buffer;
+            var numOfBytes = ((SerialPort)sender).Read(buffer, 0, 58);
+        }*/
+        public void Run()
+        {
+            createSerialPortInitial();
         }
-
     }
 }
