fbd38c67a317a7cb4e9290758b468990b09bd37e
trantrieuphi <phitran1272001@gmail.com>
2023-11-19 16:05:16 +0700

    19/11/2023

--- Simulator1/Database/LoadParameter.cs
+++ Simulator1/Database/LoadParameter.cs
@@ -12,7 +12,7 @@ namespace Simulator1.Database
 {
     public class LoadParameter
     {
-        string path = "C:\\Users\\Admin\\DAKTMT\\Simulator Software\\Simulator1\\model.json";
+        string path = "C:\\code\\DA\\APP\\App-Simulation\\Simulator1\\model.json";
         public List<ModuleObject> listInModules = new List<ModuleObject>();
         public LoadParameter()
         {
