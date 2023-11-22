e4ab6401c9cf9f1188033ca4097dec00f5eb9ff2
nguyentuananh <nguyentuananh.hcm@blueocean.net.vn>
2023-11-22 15:17:28 +0700

    add env.cs

---
+++ Environment/Env.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Environment
+{
+    public class Env
+    {
+        public const string path = "C:\\Users\\Admin\\DAKTMT\\Simulator Software\\Simulator1\\model.json";
+    }
+}
