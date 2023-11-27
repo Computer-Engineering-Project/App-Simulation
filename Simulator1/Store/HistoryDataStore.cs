dcaed5ea62794790188bce585e52be6dfd0b87b0
nguyentuananh <nguyentuananh.hcm@blueocean.net.vn>
2023-11-27 21:23:55 +0700

    recommit

--- Simulator1/Store/HistoryDataStore.cs
+++ Simulator1/Store/HistoryDataStore.cs
@@ -1,4 +1,6 @@
 ﻿using Environment.Model.History;
+using Environment.Model.Module;
+using Simulator1.State_Management;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -10,10 +12,39 @@ namespace Simulator1.Store
 {
     public class HistoryDataStore
     {
-        public List<ModuleHistory> ModuleHistories; 
-        public HistoryDataStore()
+        private readonly ModuleStore moduleStore;
+        private readonly HistoryStateManagement historyStateManagement;
+        private readonly MainStateManagement mainStateManagement;
+        public List<ModuleHistory> ModuleHistories;
+        public HistoryDataStore(ModuleStore moduleStore, HistoryStateManagement historyStateManagement, MainStateManagement mainStateManagement)
         {
-            ModuleHistories= new List<ModuleHistory>();
+            this.moduleStore = moduleStore;
+            this.historyStateManagement = historyStateManagement;
+            this.mainStateManagement = mainStateManagement;
+
+            this.historyStateManagement.ReloadHistoryData += OnReloadHistoryData;
+            this.historyStateManagement.LoadHistoryModuleFromFile += OnLoadHistoryModuleFromFile;
+            ModuleHistories = new List<ModuleHistory>();
+        }
+        public void OnReloadHistoryData(ModuleObject moduleObject)
+        {
+            var moduleHistory = ModuleHistories.FirstOrDefault(x => x.moduleObject.id == moduleObject.id);
+            if (moduleHistory != null)
+            {
+                moduleHistory.moduleObject = moduleObject;
+            }
+        }
+        public void OnLoadHistoryModuleFromFile()
+        {
+            
+            foreach(var m_object in moduleStore.ModuleObjects)
+            {
+                ModuleHistories.Clear();
+                ModuleHistories.Add(new ModuleHistory()
+                {
+                    moduleObject = m_object
+                });
+            }
         }
         public void ClearHistoryData()
         {
--- Simulator1/Store/ModuleStore.cs
+++ Simulator1/Store/ModuleStore.cs
@@ -1,4 +1,5 @@
-﻿using Environment.Model.Module;
+﻿using Environment.Model.History;
+using Environment.Model.Module;
 using Environment.Model.Packet;
 using Environment.Service.Interface;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,23 +13,26 @@ namespace Simulator1.Store
     public class ModuleStore
     {
         private readonly IServiceProvider serviceProvider;
-        private readonly LoadParameter loadParameter;
+        private readonly LoadHistoryFile loadParameter;
         private readonly MainStateManagement mainStateManagement;
+        private readonly HistoryStateManagement historyStateManagement;
 
         public List<ModuleObject> ModuleObjects { get; set; }
-        public List<ModuleObject> ModuleObjectDBs { get; set; }
         public List<string> Ports { get; set; }
-        public ModuleStore(IServiceProvider serviceProvider, LoadParameter loadParameter, MainStateManagement mainStateManagement)
+        public ModuleStore(IServiceProvider serviceProvider, LoadHistoryFile loadParameter, MainStateManagement mainStateManagement, 
+            HistoryStateManagement historyStateManagement)
         {
             this.serviceProvider = serviceProvider;
             this.loadParameter = loadParameter;
             this.mainStateManagement = mainStateManagement;
+            this.historyStateManagement = historyStateManagement;
+
+
 
             this.mainStateManagement.LoadHistory += OnLoadHistory;
 
             ModuleObjects = new List<ModuleObject>();
-            ModuleObjectDBs= new List<ModuleObject>(loadParameter.listInModules);
-            Ports= new List<string>();
+            Ports = new List<string>();
             /*var enviromentService = serviceProvider.GetRequiredService<IEnvironmentService>();
             Ports = new List<string>(enviromentService.getPorts());*/
         }
@@ -45,7 +49,7 @@ namespace Simulator1.Store
                     module.type = type;
                 }
             }*/
-            foreach (var module in ModuleObjectDBs)
+            foreach (var module in loadParameter.listInModules)
             {
                 if (module.id == id && module.type == type)
                 {
@@ -54,9 +58,18 @@ namespace Simulator1.Store
             }
             return null;
         }
+        public void Reset()
+        {
+            ModuleObjects.Clear();
+        }
         private void OnLoadHistory()
         {
-            ModuleObjects = ModuleObjectDBs;
+            if (loadParameter.ScanJsonFile())
+            {
+                mainStateManagement.resetAll();
+                ModuleObjects = loadParameter.listInModules;
+                historyStateManagement.loadHistoryModuleFromFile();
+            }
         }
     }
 }
